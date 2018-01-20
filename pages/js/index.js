6179dc5a495178b1b31feb0cb1604405c9e48d92
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2018-01-20 16:41:12 +0100

    Add multer

--- pages/js/index.js
+++ pages/js/index.js
@@ -1,17 +1,12 @@
 document.getElementById('form').addEventListener('submit', function(event){
   event.preventDefault()
+  let form = new FormData()
   let fileFront = document.getElementById('fileInput')
   let titleFront = document.getElementById('title')
-  console.log('file',fileFront.files[0])
-  console.log('title', titleFront.value)
+  form.append('fileFront', fileFront.files[0])
+  // form.append('title', titleFront.value)
   fetch('http://localhost:4000/upload', {
     method: 'post',
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      title: titleFront.value,
-      file: fileFront.files[0]
-    })
+    body: form
   })
 })
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const multer = require('multer')
 
-
+const upload = multer({ dest: 'pages/images/' })
 const app = express()
 
 app.use(express.static('pages'));
@@ -16,8 +17,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/pages/index.html'))
 })
 
-app.post('/upload', (req, res) => {
-  console.log(req.body)
+app.post('/upload', upload.single('fileFront'), (req, res) => {
+  console.log(req.body.fileFront)
   console.log(req.files)
   res.end()
 })
