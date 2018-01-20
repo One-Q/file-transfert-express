ad005e73b5a7f2081cf9a5b7daab825e95f47642
Gilmart Quentin <quentin.gilmart@student.vinci.be>
2018-01-20 15:48:43 +0100

    Front and Routes

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 
 const app = express()
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.json({
-    success: 'ok'
-  })
+  res.sendFile(path.join(__dirname + '/pages/index.html'))
+})
+
+app.post('/upload', (req, res) => {
+  console.log(req.body)
+  console.log(req.files)
+  res.end()
 })
 
 app.listen(4000, () => {
